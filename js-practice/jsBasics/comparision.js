2bdbab30500cad8619032d4e8a13293d1109ea34
Raees Khan <rk7523505@gmail.com>
2026-03-20 03:53:02 +0500

    comparision operaters

---
+++ js-practice/jsBasics/comparision.js
@@ -0,0 +1,17 @@
+// console.log(1>2); false
+// console.log(1<2); true
+// console.log(2<=3); true
+// console.log(2!= 3); true
+
+console.log("2" > 1);
+console.log(2 > "1");
+
+// ==
+console.log(2 == 2);
+
+// ===
+
+console.log("2" === 2);
+console.log(4 === 4);
+console.log(2 === 3);
+
