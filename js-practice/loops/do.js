cc1145ad5cbba6f4ed8411910c97806c77df7271
Raees Khan <rk7523505@gmail.com>
2026-05-16 14:23:23 +0500

    while and do-while loops

---
+++ js-practice/loops/do.js
@@ -0,0 +1,13 @@
+let index = 0;
+
+while (index <= 10) {
+  //   console.log(`Value of index is ${index}`);
+  index = index + 2; // it will increment the index by 2 in each iteration
+}
+
+//******************************* */
+let score = 0;
+do {
+  console.log(`Score is ${score}`);
+  score++; // it will increment the score by 1 in each iteration
+} while (score < 10); // it will execute the block of code at least once even if the condition is false because the condition is checked after the block of code is executed.
