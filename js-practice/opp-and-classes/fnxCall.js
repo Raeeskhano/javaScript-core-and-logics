e72680a541acbc0022433bf50012497f50c58248
Raees Khan <rk7523505@gmail.com>
2026-06-19 17:12:37 +0500

    calling function from another function using this keyword

---
+++ js-practice/opp-and-classes/fnxCall.js
@@ -0,0 +1,15 @@
+function setUserName(username) {
+  this.username = username;
+  console.log("called");
+}
+
+function createUser(username, email, password) {
+  setUserName.call(this, username);
+
+  this.email = email;
+  this.password = password;
+}
+
+const me = new createUser("Raees", "rk@gmail.com", "1234");
+
+console.log(me);
